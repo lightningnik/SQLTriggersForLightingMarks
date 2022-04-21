0cd836200367c21d299e2ef032bbaf45c93a90bd
lightningnik <73239458+lightningnik@users.noreply.github.com>
2022-04-21 19:23:12 +0300

    Update Создание таблиц.sql

--- Создание таблиц.sql
+++ Создание таблиц.sql
@@ -1,38 +1,119 @@
+SET NOCOUNT ON
+GO
 
-CREATE TABLE Persons(
-ID_Person int PRIMARY KEY Identity(1,1),
-Surname varchar(50),
-Name varchar(50),
-Patronymic varchar(50),
-Role varchar(50),
-Phone_number varchar(50),
-email varchar(50) UNIQUE,
-CHECK (role = 'Студент' OR role = 'Преподаватель' OR role = 'Администратор'),
-CHECK (Phone_number like '+7[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
-CHECK (email like '%@%.%'),
-);
+set nocount    on
+set dateformat mdy
 
-CREATE TABLE Disciplines(
-ID_Discipline int PRIMARY KEY Identity(1,1),
-ID_Person int,
-Name_Discipline varchar(50),
-);
+USE master
+
+
+   CREATE DATABASE Evaluations
+        ON PRIMARY 
+            ( NAME= Evaluationsdata, 
+	      FILENAME='C:\Users\Public\Evaluationsdata.mdf', SIZE=3 MB, MAXSIZE=100, FILEGROWTH=10) 
+            LOG ON 
+             (NAME= Evaluationslog, 
+		  FILENAME='C:\Users\Public\Evaluationslog.ldf', SIZE=3MB, MAXSIZE=100, FILEGROWTH=10)
+
+CHECKPOINT
+
+GO
+
+USE Evaluations
+
+GO
+
+CREATE TABLE Students
+(
+	Student_id   INT Identity (1,1) PRIMARY KEY ,
+	Surname				varchar(20)  NOT NULL,
+	Name				varchar(20)  NOT NULL,
+	Patronymic			varchar(20)  NOT NULL,
+	Date_Of_Birth		date		 NOT NULL,
+	Phone_number		varchar(50)  NOT NULL UNIQUE,
+	Mail				varchar(50)  NOT NULL UNIQUE,
+	Password			varchar(50)	 NOT NULL UNIQUE,
+	CHECK (Phone_number like '+7[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+	CHECK (Mail like '%@%.%'),
+)
+
+GO
+
+CREATE TABLE Employees
+(
+	Employee_id   INT Identity (1,1) PRIMARY KEY ,
+	Surname				varchar(20)  NOT NULL,
+	Name				varchar(20)  NOT NULL,
+	Patronymic			varchar(20)  NOT NULL,
+	Role				varchar(20)  NOT NULL,
+	Date_Of_Birth		date		 NOT NULL,
+	Phone_number		varchar(50)  NOT NULL UNIQUE,
+	Mail				varchar(50)  NOT NULL UNIQUE,
+	Password			varchar(50)	 NOT NULL UNIQUE,
+	CHECK (Phone_number like '+7[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+	CHECK (Mail like '%@%.%'),
+	CHECK (role = 'РџСЂРµРїРѕРґР°РІР°С‚РµР»СЊ' OR role = 'РђРґРјРёРЅРёРјС‚СЂР°С‚РѕСЂ')
+)
+
+GO
+
+CREATE TABLE Disciplines
+(
+	Discipline_id  INT Identity (1,1) PRIMARY KEY ,
+	Name_Discipline	varchar(20)		NOT NULL,
+)
+
+GO
+
+CREATE TABLE Groups
+(
+	Group_id    INT Identity (1,1) PRIMARY KEY ,
+	Name_Group	varchar(20)		NOT NULL UNIQUE,
+)
+
+GO
+
+CREATE TABLE Group_List
+(
+	Student_id			INT				NOT NULL,
+		CONSTRAINT FKey_Student_id FOREIGN KEY (Student_id)
+			REFERENCES Students(Student_id) ON DELETE CASCADE  ON UPDATE CASCADE,
+	Group_id		INT	UNIQUE NOT NULL,
+	            CONSTRAINT FK_group_list_id FOREIGN KEY (Group_id) 
+				  REFERENCES Groups(Group_id) ON DELETE CASCADE  ON UPDATE CASCADE,
+				  CONSTRAINT PK_id PRIMARY KEY (Student_id ,Group_id)
+)
+
+GO
 
 CREATE TABLE Marks(
-ID_Person int,
-ID_Discipline int,
-Mark int,
-Date date
-);
+Student_id			INT				NOT NULL,
+		CONSTRAINT FKey_Student_marks_id FOREIGN KEY (Student_id)
+			REFERENCES Students(Student_id) ON DELETE CASCADE  ON UPDATE CASCADE,
+Employee_id			INT				NOT NULL,
+		CONSTRAINT FKey_Employee_id FOREIGN KEY (Employee_id)
+			REFERENCES Employees(Employee_id) ON DELETE CASCADE  ON UPDATE CASCADE,
+Discipline_id			INT				NOT NULL,
+		CONSTRAINT FKey_Discipline_id FOREIGN KEY (Discipline_id)
+			REFERENCES Disciplines(Discipline_id) ON DELETE CASCADE  ON UPDATE CASCADE,
+Mark INT NOT NULL,
+Date date NOT NULL,
+CONSTRAINT PK_marks_id PRIMARY KEY (Student_id ,Employee_id, Discipline_id)
+)
 
-CREATE TABLE Groups(
-ID_Group varchar(20) PRIMARY KEY,
-ID_Person int,
-Course int
-);
+GO
 
-CREATE TABLE Login_Data(
-ID_Person int,
-email varchar(50) UNIQUE,
-Password varchar(20) UNIQUE,
-);
+CREATE TABLE Lessons(
+Group_id		INT	UNIQUE NOT NULL,
+	            CONSTRAINT FK_group_lessons_id FOREIGN KEY (Group_id) 
+				  REFERENCES Groups(Group_id) ON DELETE CASCADE  ON UPDATE CASCADE,
+Employee_id			INT				NOT NULL,
+		CONSTRAINT FKey_Employee_id FOREIGN KEY (Employee_id)
+			REFERENCES Employees(Employee_id) ON DELETE CASCADE  ON UPDATE CASCADE,
+Discipline_id			INT				NOT NULL,
+		CONSTRAINT FKey_Discipline_id FOREIGN KEY (Discipline_id)
+			REFERENCES Disciplines(Discipline_id) ON DELETE CASCADE  ON UPDATE CASCADE,
+CONSTRAINT PK_lessons_id PRIMARY KEY (Group_id ,Employee_id, Discipline_id)
+)
+
+GO
